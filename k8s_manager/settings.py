1956be0285a537b365ad60d17ba26707304940d2
limengyu1990 <limengyu1990@163.com>
2019-01-17 20:08:49 +0800

    add deploy step

--- k8s_manager/settings.py
+++ k8s_manager/settings.py
@@ -153,7 +153,6 @@ LOGGING = {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
             'filename': '/tmp/install_k8s.log',
-            'maxBytes': 400 * 1024 * 1024,
             'formatter': 'verbose'
         },
     },
